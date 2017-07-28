d97fe5c3466ac2ba0905ff2271d22f17945a8ea8
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-07-28 10:49:25 -0300

    Adicao de um feature massa.

--- src/main/java/com/alex/HomePage.java
+++ src/main/java/com/alex/HomePage.java
@@ -11,7 +11,7 @@ public class HomePage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
-	// TODO Add any page properties or variables here adsf asd fafadsfsdfa sdf asdf asd fasd fads
+	// TODO Add any page properties or variables here
 
     /**
 	 * Constructor that is invoked when page is invoked without a session. asf ad fa fasf das
@@ -24,6 +24,6 @@ public class HomePage extends WebPage {
         // Add the simplest type of label
         add(new Label("message", "If you see this message wicket is properly configured and running"));
 
-        // TODO Add your page's components here daf asd fasdf as fadsf asd fa
+        // TODO Add your page's components here
     }
 }
--- src/main/java/com/alex/WicketApplication.java
+++ src/main/java/com/alex/WicketApplication.java
@@ -4,6 +4,7 @@ import org.apache.wicket.protocol.http.WebApplication;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * tete de comiit 3
  * 
  * @see wicket.myproject.Start#main(String[])
  */
