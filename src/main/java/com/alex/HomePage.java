cb150661bb3f53de8eea8283225d23c552296594
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-07-28 10:53:01 -0300

    sem squash

--- src/main/java/com/alex/HomePage.java
+++ src/main/java/com/alex/HomePage.java
@@ -11,7 +11,7 @@ public class HomePage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
-	// TODO Add any page properties or variables here
+	// TODO Add any page properties or variables hereads fasd fasd fas
 
     /**
 	 * Constructor that is invoked when page is invoked without a session. asf ad fa fasf das
@@ -24,6 +24,6 @@ public class HomePage extends WebPage {
         // Add the simplest type of label
         add(new Label("message", "If you see this message wicket is properly configured and running"));
 
-        // TODO Add your page's components here
+        // TODO Add your page's components here asdf asf asd fa
     }
 }
